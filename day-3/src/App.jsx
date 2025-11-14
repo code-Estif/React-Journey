dba439d9a3426f406e857c72f17e274bc17550bb
code-Estif <legendestif@gmail.com>
2025-11-14 09:12:46 +0300

    Props

---
+++ day-3/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Profile from './components/Profile'
+
+function App() {
+  return (
+    <>
+    <Profile className="profile-card" name="Estif" age="17"></Profile>
+    </>
+  )
+}
+
+export default App
---
+++ day-3/src/components/Profile.jsx
@@ -0,0 +1,10 @@
+function Profile(props) {
+    return (
+    <div className='profile-card'>
+        <p>{props.name}</p>
+        <p>{props.age}</p>
+        </div>
+        )
+    }
+
+export default Profile    
